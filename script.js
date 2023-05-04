1511b1abcb6eed87508312b5b6db5f2731cfe865
Edson102023 <santanaedson2018@gmailcom>
2023-05-04 12:40:23 -0300

    project louco

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("conect=button")
+
+const Dolar = 5.2
+
+const convertvalue = () => {
+    const imputReais = document.getElementById("imput-real").value
+    console.log(imputReais / Dolar)
+
+}
+
+
+
+
+
+
+
